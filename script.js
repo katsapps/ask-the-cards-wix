b7fa72a9e961843149c562974064efb30f6e6cf4
Kateryna S <kateryna.semenova1@gmail.com>
2025-11-08 09:57:59 -0500

    test phoenix card Set chosen_num to a fixed value of 13

--- script.js
+++ script.js
@@ -308,6 +308,7 @@ $(document).ready(function() {
             chosen_num = Math.floor(Math.random() * dict_size); //65
 			//chosen_num = Math.floor(Math.random() * 16); //65
 			//chosen_num = 0; //longest
+			chosen_num = 13;
 
             var card_name = deck_dict[chosen_num.toString()]; //deck_dict["65"] = "two-of-wands"
             var img = new Image();
